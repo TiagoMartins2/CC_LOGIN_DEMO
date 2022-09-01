8cc6e1e53032541526e77552c3e989ecbaeee490
TiagoMartins2 <tiago.martins@dnxt.pt>
2022-09-01 12:14:05 +0100

    new

---
+++ CC_LOGIN_DEMO/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <eidlib.h>
+#include <eidlibcompat.h>
+#include <eidlibcompat.h>
+#include <eidlibdefines.h>
+#include <eidlibException.h>
+#include <eidErrors.h>
+
+
+using namespace std;
+
+int main() {
+	int p;
+	do{
+		cout << "===============================" << endl;
+		cout << "           LOG IN" << endl;
+		cout << "===============================" << endl;
+		
+
+
+
+	} while (p == 0);
+	return 0;
+}
