caa3600cc3bd0d42acce66b49022d0d389eeb011
TiagoMartins2 <tiago.martins@dnxt.pt>
2022-09-02 16:48:57 +0100

    new changes, falta implementar

--- CC_LOGIN_DEMO/Source.cpp
+++ CC_LOGIN_DEMO/Source.cpp
@@ -12,6 +12,10 @@
 using namespace eIDMW;
 using namespace std;
 
+
+PTEID_ReaderContext& ReturnReader();
+
+
 int main() {
 	char user[] = "152863567";
 	string p;
@@ -60,7 +64,7 @@ int main() {
 					cout << "           MENU" << endl;
 					cout << "===============================" << endl;
 					cout << "\n\n\nWelcome " << eid.getGivenName() << " " << eid.getSurname() << endl;
-					cout << "\n\n\Press 0 to leave... " << endl;
+					cout << "\n\n\n Press 0 to leave... " << endl;
 					cout << "\n\n\n===============================" << endl;
 					cout << "===============================" << endl;
 					
@@ -90,4 +94,83 @@ int main() {
 }
 
 
+
+
+
+
+//RETURN THE SMART CARD READER CONNECTED TO THE SYSTEM
+PTEID_ReaderContext& ReturnReader() {
+	PTEID_InitSDK();
+
+	PTEID_ReaderContext& readerC = PTEID_ReaderSet::instance().getReader();
+	return readerC;
+
+	PTEID_ReleaseSDK();
+}
+
+
+
+//RETURN THE EIDCard OBJECT
+PTEID_EIDCard& ReturnEIDCard() {
+	PTEID_InitSDK();
+
+	PTEID_EIDCard& eIDCard = ReturnReader().getEIDCard();
+	return eIDCard;
+
+	PTEID_ReleaseSDK();
+}
+
+
+
+//RETURN THE eID OBJECT
+PTEID_EId& ReturnEID() {
+	PTEID_InitSDK();
+
+	PTEID_EId& eid = ReturnEIDCard().getID();
+	return eid;
+
+	PTEID_ReleaseSDK();
+}
+
+
+
+//GET ADDRESS PIN
+PTEID_Pin& ReturnADDR_PIN() {
+	PTEID_InitSDK();
+
+	PTEID_Pins& pins = ReturnEIDCard().getPins();
+	PTEID_Pin& pin = pins.getPinByPinRef(PTEID_Pin::ADDR_PIN);
+	return pin;
+
+	PTEID_ReleaseSDK();
+}
+
+
+
+//GET AUTHENTICATION PIN
+PTEID_Pin& ReturnAUTH_PIN() {
+	PTEID_InitSDK();
+
+	PTEID_Pins& pins = ReturnEIDCard().getPins();
+	PTEID_Pin& pin = pins.getPinByPinRef(PTEID_Pin::AUTH_PIN);
+	return pin;
+
+	PTEID_ReleaseSDK();
+}
+
+
+
+//CHECK IF THE PASSWORD IS CORRECT 
+//IF RETURN 1 IS FALSE 
+//IF RETURN 0 IS TRUE
+bool checkUser(char pinInserted[100]) {
+	PTEID_InitSDK();
+
+	PTEID_Pin& pinAUTH = ReturnAUTH_PIN();
+	unsigned long triesLeft;
+	bool a = pinAUTH.verifyPin(pinInserted, triesLeft, true);
+	return a;
+
+	PTEID_ReleaseSDK();
+}
 
